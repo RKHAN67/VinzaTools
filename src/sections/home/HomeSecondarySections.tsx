951ca04a964a3a312f459389df029547fb4fe532
Rizwan Khan <rizwan819101@gmail.com>
2026-04-17 12:49:04 +0500

    Fix HomeSecondarySections devSubtools crash

--- src/sections/home/HomeSecondarySections.tsx
+++ src/sections/home/HomeSecondarySections.tsx
@@ -532,7 +532,7 @@ export const HomeSecondarySections = ({
           </div>
 
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 relative z-10">
-            {devSubtools.map((tool) => (
+            {DEV_SUBTOOLS.map((tool) => (
               <button
                 key={tool.id}
                 onClick={() => openSubTool('dev-tools', tool.id)}
@@ -572,7 +572,7 @@ export const HomeSecondarySections = ({
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {mediaSubtools.map((tool, index) => (
+          {MEDIA_SUBTOOLS.map((tool, index) => (
             <button
               key={tool.id}
               onClick={() => openSubTool('mediaflow-downloader', tool.id)}
